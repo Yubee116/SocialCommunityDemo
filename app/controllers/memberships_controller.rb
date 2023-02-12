4fb2cc687428db396b936b389cc718be138e183a
Ubong Nehemiah <ubongnehemiah@gmail.com>
2023-02-12 14:37:19 +0100

    fixed redirect after on group join

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -11,7 +11,7 @@ class MembershipsController < ApplicationController
 
         respond_to do |format|
             if @membership.save
-                format.html { redirect_to group_url(@membership), notice: "You have joined the group." }
+                format.html { redirect_to group_url(@membership.group), notice: "You have joined the group." }
                 format.json { render :show, status: :created, location: @membership }
             else
                 format.html { redirect_to groups_url, alert: "Unable to join. You are probably already a member of this group" }
